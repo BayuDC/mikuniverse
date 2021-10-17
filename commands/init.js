b510bfff9f7285f5b340fbdb035de248ff725b4f
BayuDC <dev.bayudc@gmail.com>
2021-10-18 06:12:31 +0700

    Improve init command

--- commands/init.js
+++ commands/init.js
@@ -1,11 +1,23 @@
-const mikuniverse = require('../lib/mikuniverse');
+const Category = require('../models/category');
 
 module.exports = {
     name: 'init',
     async execute(message, name) {
-        await mikuniverse.init(name, message.channel.id);
+        const category = await Category.findOne({ $or: [{ name }, { channel: message.channel.id }] });
+        if (category) {
+            if (category.name == name) {
+                await message.channel.send(`Category **${name}** already exists`);
+            }
+            if (category.channel == message.channel.id) {
+                await message.channel.send('This channel is already in use');
+            }
+            return;
+        }
 
+        await Category.create({ name, channel: message.channel.id });
         message.client.mikuChannels.set(name, message.channel);
-        message.channel.send('Success');
+        // TODO set permissions
+
+        await message.channel.send(`Category **${name}** successfully created`);
     },
 };
